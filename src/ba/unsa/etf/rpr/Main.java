d10aa7f3239fc3854bc27db1a9d0bc5c1b4081bd
acatic2 <acatic2@etf.unsa.ba>
2019-10-14 15:29:37 +0200

    Zadatak 2

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,25 @@
+package ba.unsa.etf.rpr;
+import java.util.Scanner;
+
+public class Main {
+
+    public static int sumaCifara(int n){
+        int broj=n;
+        int suma=0;
+        while(broj!=0){
+            suma+=broj%10;
+            broj/=10;
+        }
+        return suma;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Unesite n: ");
+        Scanner ulaz=new Scanner(System.in);
+        int n=ulaz.nextInt();
+        for(int i=1;i<=n;i++){
+            if(i%sumaCifara(i)==0) System.out.println(i);
+        }
+    }
+}
